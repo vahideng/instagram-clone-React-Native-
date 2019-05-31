56571675d8e81137b282164fbc96e9ea513169bc
Vahid Davoudi <vahid_davoudi_e@yahoo.com>
2019-06-01 07:38:00 +0800

    First commit

---
+++ components/bananas.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import {Image} from 'react-native';
+
+
+class Banan extends Component {
+
+    render() {
+return(
+
+   <Image source = {{uri : 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}} 
+   
+   style={{width: 193, height: 110}}
+   />
+
+)
+
+    }
+}
+
+export default Banan
